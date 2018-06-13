77471c3637d560a4b7f37790ce2dff321237b188
viv <vivian_pr@hotmail.com>
2018-06-13 08:56:57 +0100

    refactored to vue.js

---
+++ src/fullPageMixin.js
@@ -0,0 +1,31 @@
+import * as constants from './constants';
+
+
+export default {
+  data() {
+    return {
+      $_fullPageMixin_refName: 'fullPage',
+    };
+  },
+
+  methods: {
+    // Methods of Full Page
+    ...constants.METHODS.reduce((methods, method) => {
+      return {
+        ...methods,
+
+        [method]: (...args) => {
+          if (args[0] instanceof Event) {
+            args.shift();
+          }
+
+          if (args.length) {
+            $.fn.fullpage[method].apply( this, args );
+          } else {
+            $.fn.fullpage[method]();
+          }
+        }
+      };
+    }, {}),
+  }
+};
